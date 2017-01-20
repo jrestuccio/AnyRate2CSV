125c162528eba6bf8a2b541fd6204dbe7862eeb9
jrestuccio <jstoosh84@gmail.com>
2017-01-20 00:00:55 -0600

    Initial Form Creation

---
+++ AnyRate2CSV/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace AnyRate2CSV
+{
+  partial class AnyRate2CSV
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.label1 = new System.Windows.Forms.Label();
+      this.btnSettings = new System.Windows.Forms.Button();
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.button3 = new System.Windows.Forms.Button();
+      this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+      this.textBox1 = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.textBox2 = new System.Windows.Forms.TextBox();
+      this.button1 = new System.Windows.Forms.Button();
+      this.label4 = new System.Windows.Forms.Label();
+      this.textBox3 = new System.Windows.Forms.TextBox();
+      this.label5 = new System.Windows.Forms.Label();
+      this.textBox4 = new System.Windows.Forms.TextBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.PYRSoldOutPlusOne = new System.Windows.Forms.GroupBox();
+      this.textBox5 = new System.Windows.Forms.TextBox();
+      this.label6 = new System.Windows.Forms.Label();
+      this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
+      this.PYRSoldOutPlusOne.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label1.Location = new System.Drawing.Point(136, 9);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(204, 31);
+      this.label1.TabIndex = 1;
+      this.label1.Text = "AnyRate 2 CSV";
+      this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.label1.Click += new System.EventHandler(this.label1_Click);
+      // 
+      // btnSettings
+      // 
+      this.btnSettings.ForeColor = System.Drawing.Color.DarkBlue;
+      this.btnSettings.Location = new System.Drawing.Point(26, 19);
+      this.btnSettings.Name = "btnSettings";
+      this.btnSettings.Size = new System.Drawing.Size(97, 25);
+      this.btnSettings.TabIndex = 2;
+      this.btnSettings.Text = "Settings";
+      this.btnSettings.UseVisualStyleBackColor = true;
+      this.btnSettings.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Controls.Add(this.button1);
+      this.groupBox1.Controls.Add(this.button3);
+      this.groupBox1.Controls.Add(this.btnSettings);
+      this.groupBox1.Location = new System.Drawing.Point(469, 67);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(152, 181);
+      this.groupBox1.TabIndex = 3;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+      // 
+      // button3
+      // 
+      this.button3.ForeColor = System.Drawing.Color.DarkBlue;
+      this.button3.Location = new System.Drawing.Point(26, 60);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(97, 25);
+      this.button3.TabIndex = 4;
+      this.button3.Text = "Start Extract";
+      this.button3.UseVisualStyleBackColor = true;
+      // 
+      // monthCalendar1
+      // 
+      this.monthCalendar1.Location = new System.Drawing.Point(18, 86);
+      this.monthCalendar1.Name = "monthCalendar1";
+      this.monthCalendar1.TabIndex = 4;
+      // 
+      // textBox1
+      // 
+      this.textBox1.Location = new System.Drawing.Point(110, 45);
+      this.textBox1.Name = "textBox1";
+      this.textBox1.Size = new System.Drawing.Size(59, 20);
+      this.textBox1.TabIndex = 5;
+      this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label2.Location = new System.Drawing.Point(110, 26);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(59, 13);
+      this.label2.TabIndex = 6;
+      this.label2.Text = "Days Back";
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label3.Location = new System.Drawing.Point(15, 26);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(75, 13);
+      this.label3.TabIndex = 8;
+      this.label3.Text = "Spree Number";
+      // 
+      // textBox2
+      // 
+      this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+      this.textBox2.Location = new System.Drawing.Point(15, 45);
+      this.textBox2.Name = "textBox2";
+      this.textBox2.Size = new System.Drawing.Size(75, 20);
+      this.textBox2.TabIndex = 7;
+      // 
+      // button1
+      // 
+      this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+      this.button1.Location = new System.Drawing.Point(26, 106);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(97, 25);
+      this.button1.TabIndex = 5;
+      this.button1.Text = "Clear V2 Queue";
+      this.button1.UseVisualStyleBackColor = true;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label4.Location = new System.Drawing.Point(188, 26);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(40, 13);
+      this.label4.TabIndex = 10;
+      this.label4.Text = "Retries";
+      // 
+      // textBox3
+      // 
+      this.textBox3.Location = new System.Drawing.Point(196, 45);
+      this.textBox3.Name = "textBox3";
+      this.textBox3.Size = new System.Drawing.Size(21, 20);
+      this.textBox3.TabIndex = 9;
+      this.textBox3.Text = "2";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label5.Location = new System.Drawing.Point(246, 26);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(66, 13);
+      this.label5.TabIndex = 12;
+      this.label5.Text = "Fresh Factor";
+      // 
+      // textBox4
+      // 
+      this.textBox4.Location = new System.Drawing.Point(249, 45);
+      this.textBox4.Name = "textBox4";
+      this.textBox4.Size = new System.Drawing.Size(40, 20);
+      this.textBox4.TabIndex = 11;
+      this.textBox4.Text = "0";
+      // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.button2);
+      this.groupBox2.Controls.Add(this.label2);
+      this.groupBox2.Controls.Add(this.label5);
+      this.groupBox2.Controls.Add(this.textBox1);
+      this.groupBox2.Controls.Add(this.textBox4);
+      this.groupBox2.Controls.Add(this.textBox2);
+      this.groupBox2.Controls.Add(this.label4);
+      this.groupBox2.Controls.Add(this.label3);
+      this.groupBox2.Controls.Add(this.textBox3);
+      this.groupBox2.Location = new System.Drawing.Point(18, 298);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(338, 140);
+      this.groupBox2.TabIndex = 13;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "PYR Create Queue";
+      // 
+      // button2
+      // 
+      this.button2.ForeColor = System.Drawing.Color.DarkBlue;
+      this.button2.Location = new System.Drawing.Point(18, 89);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(132, 23);
+      this.button2.TabIndex = 13;
+      this.button2.Text = "Start PYR Queue";
+      this.button2.UseVisualStyleBackColor = true;
+      // 
+      // PYRSoldOutPlusOne
+      // 
+      this.PYRSoldOutPlusOne.Controls.Add(this.textBox5);
+      this.PYRSoldOutPlusOne.Controls.Add(this.label6);
+      this.PYRSoldOutPlusOne.Location = new System.Drawing.Point(18, 455);
+      this.PYRSoldOutPlusOne.Name = "PYRSoldOutPlusOne";
+      this.PYRSoldOutPlusOne.Size = new System.Drawing.Size(169, 143);
+      this.PYRSoldOutPlusOne.TabIndex = 14;
+      this.PYRSoldOutPlusOne.TabStop = false;
+      this.PYRSoldOutPlusOne.Text = "PYR Sold Out Plus One";
+      this.PYRSoldOutPlusOne.Enter += new System.EventHandler(this.groupBox3_Enter);
+      // 
+      // textBox5
+      // 
+      this.textBox5.BackColor = System.Drawing.SystemColors.Window;
+      this.textBox5.Location = new System.Drawing.Point(15, 44);
+      this.textBox5.Name = "textBox5";
+      this.textBox5.Size = new System.Drawing.Size(135, 20);
+      this.textBox5.TabIndex = 9;
+      // 
+      // label6
+      // 
+      this.label6.AutoSize = true;
+      this.label6.ForeColor = System.Drawing.Color.RoyalBlue;
+      this.label6.Location = new System.Drawing.Point(15, 25);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(58, 13);
+      this.label6.TabIndex = 10;
+      this.label6.Text = "Session ID";
+      // 
+      // AnyRate2CSV
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.SystemColors.ButtonFace;
+      this.ClientSize = new System.Drawing.Size(995, 729);
+      this.Controls.Add(this.PYRSoldOutPlusOne);
+      this.Controls.Add(this.groupBox2);
+      this.Controls.Add(this.monthCalendar1);
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.label1);
+      this.ForeColor = System.Drawing.Color.CornflowerBlue;
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+      this.Name = "AnyRate2CSV";
+      this.Text = "AnyRate 2 CSV";
+      this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+      this.Load += new System.EventHandler(this.AnyRate2CSV_Load);
+      this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
+      this.groupBox2.PerformLayout();
+      this.PYRSoldOutPlusOne.ResumeLayout(false);
+      this.PYRSoldOutPlusOne.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Button btnSettings;
+    private System.Windows.Forms.GroupBox groupBox1;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.MonthCalendar monthCalendar1;
+    private System.Windows.Forms.TextBox textBox1;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox textBox2;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox textBox3;
+    private System.Windows.Forms.Label label5;
+    private System.Windows.Forms.TextBox textBox4;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.GroupBox PYRSoldOutPlusOne;
+    private System.Windows.Forms.TextBox textBox5;
+    private System.Windows.Forms.Label label6;
+  }
+}
+
